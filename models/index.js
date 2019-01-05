4daab9e8e5233da2df1dd6d0078eed292946db40
Phoenix <aja.magdaleno@gmail.com>
2019-01-05 14:11:33 -0800

    with typo fixes

--- models/index.js
+++ models/index.js
@@ -2,9 +2,6 @@
 module.exports = {
   User: require("./User"),
   Meds: require("./Meds"),
-<<<<<<< HEAD
-  Patient: require("./Patient")
-=======
+  Patient: require("./Patient"),
   Doctor: require("./Doctor")
->>>>>>> 52690cb8984890f6f30c79ca85819bd9de1a6b69
 }
