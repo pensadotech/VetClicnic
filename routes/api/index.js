e89ebaa6ee7dc5fd982df0a97cba1d2e37081f67
Phoenix <aja.magdaleno@gmail.com>
2019-01-08 18:30:03 -0800

    trying to pull

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,11 +8,7 @@ const appointmentRoutes = require('./appointmentRoutes');
 // Matches with "/api/session"
 router.use('/session', authRoutes);
 // Matches with "/api/users"
-<<<<<<< HEAD
-router.use('/users', usersRoutes)
-=======
 router.use('/users', usersRoutes);
->>>>>>> 287a60526847f5db3198a2fcc540423eac6f9071
 // Matches with "/api/patients"
 router.use('/patients', patientsRoutes);
 // Matches with "/api/doctors"
@@ -20,11 +16,6 @@ router.use('/doctors', doctorsRoutes);
 // Matches with "/api/appointment"
 router.use('/appointment', appointmentRoutes);
 
-<<<<<<< HEAD
-
-router.use('./doctors', doctorsRoutes)
-=======
->>>>>>> 287a60526847f5db3198a2fcc540423eac6f9071
 // Other routes goes here
 
 module.exports = router;
