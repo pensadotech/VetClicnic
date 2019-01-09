7443716c239bad861d84bf2223df38c5c3ea8516
mnkyhdx <mnkyhdx@dhcp-v070-179.mobile.uci.edu>
2019-01-08 17:58:03 -0800

    DoctorCard incomplete

---
+++ client/src/components/Doctor/components/DoctorCard/DoctorCard.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react"
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const styles = {
+  card: {
+    minWidth: 275,
+    maxHeight: 220,
+    margin: '10px 20px 0px 20px',  
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+}
+
+class DoctorCard extends Component { 
+  
+  render() {
+  
+    const { classes } = this.props
+  
+    return (
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+          System user
+          </Typography>
+          <Typography variant="h5" component="h2">
+            {this.props.user.username}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            {this.props.user.fullname}
+          </Typography>
+          <Typography component="p">
+            'Phone: ' {this.props.user.phone}
+          </Typography>
+          <Typography component="p">
+            'Email: ' {this.props.user.email}
+          </Typography>
+        </CardContent>
+        <CardActions>     
+          <Button size="small" variant="contained" color={this.props.rightbuttonColor} disabled={this.props.isDisabled}
+                  onClick={() => this.props.handleRightButtonSelection(this.props.user)} >{this.props.rightButtonLabel}</Button>
+          <Button size="small" variant="contained" color={this.props.leftbuttonColor} disabled={this.props.isDisabled}  
+                  onClick={() => this.props.handleLeftButtonSelection(this.props.user)}>{this.props.leftButtonLabel}</Button>
+        </CardActions>
+      </Card>
+    )
+  } 
+}
+
+UserCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(UserCard)
