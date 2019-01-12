ef7cc6d8529afbca4adc3010ba055e38217ea2c0
mnkyhdx <mnkyhdx@dhcp-v126-254.mobile.uci.edu>
2019-01-12 11:45:03 -0800

    Another Doctor Component Update

--- client/src/components/Doctor/Doctor.js
+++ client/src/components/Doctor/Doctor.js
@@ -10,7 +10,7 @@ import AddIcon from '@material-ui/icons/Add';
 
 // Components
 import DoctorCard from './components/DoctorCard'
-import DoctorForm from '../Doctor/components/DoctorForm/DoctorForm'
+import DoctorForm from './components/DoctorForm'
 
 // API
 import APIdoctors from '../../utils/APIdoctor'
@@ -53,35 +53,34 @@ class Doctor extends Component {
       .catch(err => console.log(err))
   }
   
-  handleMedAddSelection = () => {
+  handleDoctorAddSelection = () => {
     // change screen mode to user ADD mode
     this.setState({ screenMode: 'add' })
   }
   
-  handleMedUpdateSelection = (tgtDoctor) => {
+  handleDoctorUpdateSelection = (tgtDoctor) => {
     // change screen mode to user EDIT mode, and store target-user
     this.setState({ screenMode: 'edit', targetDoctor: tgtDoctor })
   }
   
-  handleMedDeleteSelection = (tgtDoctor) => {
+  handleDoctorDeleteSelection = (tgtDoctor) => {
     // Change screen mode to User DELETE mode, and store target-user
     this.setState({ screenMode: 'delete', targetDoctor: tgtDoctor })
   }
   handleCreateDoctor =(tgtDoctor) => {
-     
-    // create new user
-    APIdoctors.createUpdateMed(tgtDoctor)
-      .then(r => {       
-        // Restore main view
-        this.setState({screenMode: 'list',targetDoctor: ''}) 
-        // reload the data
-        this.loadDoctors()
-      })
-      .catch(err => console.log(err))
+    console.log(tgtDoctor)
+  //create new user
+  //   APIdoctors.createUpdateDoctor(tgtDoctor)
+  //     .then(r => {       
+  //       // Restore main view
+  //       this.setState({screenMode: 'list',targetDoctor: ''}) 
+  //       // reload the data
+  //       this.loadDoctors()
+  //     })
+  //     .catch(err => console.log(err))
   }
 
   handleSaveDoctor = (tgtDoctor) => {
-
     // Save updated user data    
     APIdoctors.updateDoctor(tgtDoctor._id,tgtDoctor)
       .then(r => {  
@@ -121,14 +120,14 @@ class Doctor extends Component {
       return(
         <>
           <h1 className={classes.pageHead}>
-            Add new medicine
+            Add new Doctor
           </h1>
           <DoctorForm 
-             mode={this.state.screenMode}
-            med=''
+            mode={this.state.screenMode}
+            doctor=''
             leftbuttonColor='primary'
             leftButtonLabel='Add'
-            handleLeftButtonSelection={this.handleCreateMed}
+            handleLeftButtonSelection={this.handleCreateDoctor}
             rightbuttonColor='default'
             rightButtonLabel='Cancel'   
             handleRightButtonSelection={this.handleCancel}
@@ -142,7 +141,7 @@ class Doctor extends Component {
       return(
         <>
           <h1 className={classes.pageHead}>
-            Update medicine information
+            Update doctor information
           </h1>
           <DoctorForm 
             mode={this.state.screenMode}
@@ -153,7 +152,7 @@ class Doctor extends Component {
             rightbuttonColor='default'
             rightButtonLabel='Cancel'   
             handleRightButtonSelection={this.handleCancel}
-            isNameDisabled={true}
+            isNameDisabled={false}
           />
         </>
       ) // return()
@@ -164,7 +163,7 @@ class Doctor extends Component {
         <>
           <div>
             <h1 className={classes.pageHead}>
-              Do you want to delete this Medicine?
+              Do you want to remove this Doctor?
             </h1>
             <DoctorCard 
                    doctor={this.state.targetDoctor}
@@ -200,7 +199,9 @@ class Doctor extends Component {
             </Grid>
           </Grid>
         
-          <Grid>
+          <Grid alignContent='center'
+            style={{ margin: 'auto', minHeight: '94vh', marginLeft: '5%' }}
+            container spacing={32}>
             {
               this.state.doctors.map((doctor, index) => (
                  <DoctorCard 
@@ -208,11 +209,11 @@ class Doctor extends Component {
                    doctor={doctor}
                    leftbuttonColor='primary'
                    leftButtonLabel='Update'
-                   handleLeftButtonSelection={this.handleUpdateSelection}
+                   handleLeftButtonSelection={this.handleDoctorUpdateSelection}
                    rightbuttonColor='secondary'
                    rightButtonLabel='Remove'
                   isDisabled={false}
-                  handleRightButtonSelection={this.handleMedDeleteSelection}
+                  handleRightButtonSelection={this.handleDoctorDeleteSelection}
                  />
               ))
             }
